63572401d46646e1d884a618712b2ad0c98797d8
Jorge Cisneros <fraxito@gmail.com>
2016-04-19 11:45:18 +0200

    listener

--- app/src/main/java/com/example/xp/repostator/MainActivity.java
+++ app/src/main/java/com/example/xp/repostator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.xp.repostator;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,4 +59,29 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        SharedPreferences sharedPref = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        ArrayList <String> listadoRepostajes = new ArrayList<String>();
+
+        int size = sharedPref.getInt("listado_size", 0);
+        //leo los elementos del sharedPref y los guardo en el arraylist
+        for (int i = 1; i<=size; i++){
+            listadoRepostajes.add(sharedPref.getString("repostaje_"+i, ""));
+        }
+
+        ListView lista  = (ListView) findViewById(R.id.marcoLista);
+
+
+        //el array adapter
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listadoRepostajes
+        );
+        lista.setAdapter(arrayAdapter);
+    }
+
 }
