9b01f776abd435aa6044e3cacdcd0f21c7edb67e
Jorge Cisneros <fraxito@gmail.com>
2016-04-21 10:41:14 +0200

    Inicio Jueves

--- app/src/main/java/com/example/xp/repostator/ActualizaDatos.java
+++ app/src/main/java/com/example/xp/repostator/ActualizaDatos.java
@@ -41,6 +41,31 @@ public class ActualizaDatos extends AppCompatActivity {
 
     }
 
+    private void escribeDatos(){
+        EditText editPrecio = (EditText) findViewById(R.id.precio);
+        EditText editKilometos = (EditText) findViewById(R.id.kilometros);
+        EditText editLitros = (EditText) findViewById(R.id.litros);
+        DatePicker editFecha = (DatePicker) findViewById(R.id.fecha);
+        SharedPreferences sharedPref = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        int size = sharedPref.getInt("listado_size", 0);
+        size++;
+
+        editor.putString("precio_" + size, editPrecio.getText().toString());
+        editor.putString("kilometros_" + size, editKilometos.getText().toString());
+        editor.putString("litros_" + size, editLitros.getText().toString());
+
+        editor.putInt("dia_" + size, editFecha.getDayOfMonth());
+        editor.putInt("mes_" + size, editFecha.getMonth());
+        editor.putInt("year_" + size, editFecha.getYear());
+
+        editor.putInt("listado_size", size);
+
+        editor.commit();
+
+    }
+
     @Override
     public void onStart(){
         super.onStart();
@@ -52,6 +77,19 @@ public class ActualizaDatos extends AppCompatActivity {
 
             leeDatos(posicion);
         }
+
+        final EditText editPrecio = (EditText) findViewById(R.id.precio);
+        editPrecio.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    escribeDatos();
+                    finish();
+                }
+                return false;
+            }
+        });
+
     }
 
 }
